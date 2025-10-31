e377e49f97ee988945c360853206a30f033e69ff
Yohei <74522538+YohyYamasaki@users.noreply.github.com>
2025-10-31 16:46:50 +0100

    Implement parser & codegen

---
+++ src/engine/codegen.rs
@@ -0,0 +1,183 @@
+use std::{
+    error::Error,
+    fmt::{self, Display},
+};
+
+use crate::{
+    engine::{Instruction, parser::AST},
+    helper::safe_add,
+};
+
+#[derive(Debug)]
+pub enum CodeGenError {
+    PCOverFlow,
+    FailStar,
+    FailOr,
+    FailQuestion,
+}
+
+impl Display for CodeGenError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "CodeGenError: {:?}", self)
+    }
+}
+
+impl Error for CodeGenError {}
+
+#[derive(Default, Debug)]
+struct Generator {
+    pc: usize,
+    insts: Vec<Instruction>,
+}
+
+impl Generator {
+    fn inc_pc(&mut self) -> Result<(), CodeGenError> {
+        safe_add(&mut self.pc, &1, || CodeGenError::PCOverFlow)
+    }
+
+    fn gen_expr(&mut self, ast: &AST) -> Result<(), CodeGenError> {
+        match ast {
+            AST::Char(c) => self.gen_char(*c)?,
+            AST::Or(e1, e2) => self.gen_or(e1, e2)?,
+            AST::Plus(e) => self.gen_plus(e)?,
+            AST::Star(e) => self.gen_star(e)?,
+            AST::Question(e) => self.gen_question(e)?,
+            AST::Seq(v) => self.gen_seq(v)?,
+        }
+        Ok(())
+    }
+
+    fn gen_seq(&mut self, exprs: &[AST]) -> Result<(), CodeGenError> {
+        for e in exprs {
+            self.gen_expr(e)?;
+        }
+        Ok(())
+    }
+
+    fn gen_char(&mut self, c: char) -> Result<(), CodeGenError> {
+        self.insts.push(Instruction::Char(c));
+        self.inc_pc()?;
+        Ok(())
+    }
+
+    /// OR operator code generator.
+    /// The result will look like below.
+    ///
+    /// ```text
+    ///			split L1, L2
+    /// L1: e1 code
+    /// 		jmp L3
+    /// L2: e2 code
+    /// L3: ...
+    /// ```
+    fn gen_or(&mut self, e1: &AST, e2: &AST) -> Result<(), CodeGenError> {
+        // split L1, L2
+        let split_addr = self.pc;
+        self.inc_pc()?;
+        let split = Instruction::Split(self.pc, 0); // L1 = self.pc
+        self.insts.push(split);
+
+        // L1: e1 code
+        self.gen_expr(e1)?;
+
+        // jmp L3
+        let jmp_addr = self.pc;
+        self.insts.push(Instruction::Jump(0)); // 0 is temp address for L3
+
+        // Set L2 address, which is intialized with 0
+        self.inc_pc()?;
+        if let Some(Instruction::Split(_, l2)) = self.insts.get_mut(split_addr) {
+            *l2 = self.pc;
+        } else {
+            return Err(*Box::new(CodeGenError::FailOr));
+        }
+
+        // L2: e2 code
+        self.gen_expr(e2)?;
+
+        // Set L3 value
+        if let Some(Instruction::Jump(l3)) = self.insts.get_mut(jmp_addr) {
+            *l3 = self.pc;
+        } else {
+            return Err(*Box::new(CodeGenError::FailOr));
+        }
+        Ok(())
+    }
+
+    /// Generate code for the + operator.
+    ///
+    ///  ```text
+    /// L1: code of e
+    /// 		split L1, L2
+    /// L2: ...
+    /// ```
+    fn gen_plus(&mut self, e: &AST) -> Result<(), CodeGenError> {
+        let l1 = self.pc;
+        self.gen_expr(e)?;
+        self.inc_pc()?;
+        self.insts.push(Instruction::Split(l1, self.pc));
+        Ok(())
+    }
+
+    /// Generate code for the * operator.
+    ///
+    /// ```text
+    /// L1: split L1, L2
+    /// L2: code of e
+    /// 	  jmp L1
+    /// L3: ...
+    /// ```
+    fn gen_star(&mut self, e: &AST) -> Result<(), CodeGenError> {
+        let l1 = self.pc;
+        self.insts.push(Instruction::Split(self.pc, 0)); // L2 = self.pc, L3 starts from 0
+
+        // L2: generate the code for e
+        self.gen_expr(e)?;
+
+        // Jump to L1
+        self.inc_pc()?;
+        self.insts.push(Instruction::Jump(l1));
+
+        // Set L3 address
+        if let Some(Instruction::Split(_, l3)) = self.insts.get_mut(l1) {
+            *l3 = self.pc;
+            Ok(())
+        } else {
+            Err(CodeGenError::FailStar)
+        }
+    }
+
+    /// Generate code for the ? operator.
+    ///
+    /// ```text
+    /// 		split L1, L2
+    /// L1: code of e
+    /// L2: ...
+    /// ```
+    fn gen_question(&mut self, e: &AST) -> Result<(), CodeGenError> {
+        let split_addr = self.pc;
+        self.inc_pc()?;
+        self.insts.push(Instruction::Split(self.pc, 0)); // L2 starts from 0
+        self.gen_expr(e)?;
+
+        if let Some(Instruction::Split(_, l2)) = self.insts.get_mut(split_addr) {
+            *l2 = self.pc;
+            Ok(())
+        } else {
+            Err(CodeGenError::FailQuestion)
+        }
+    }
+
+    fn gen_code(&mut self, ast: &AST) -> Result<(), CodeGenError> {
+        self.gen_expr(ast)?;
+        self.inc_pc()?;
+        self.insts.push(Instruction::Match);
+        Ok(())
+    }
+}
+
+fn get_code(ast: &AST) -> Result<Vec<Instruction>, CodeGenError> {
+    let mut generator = Generator::default();
+    generator.gen_code(ast)?;
+    Ok(generator.insts)
+}
